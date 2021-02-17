af05847f2f07090b93d69f102b77d918ae9e323e
PatoGordo <icariusv@gmail.com>
2021-02-17 03:28:28 +0000

    bars animated icon

--- Src/Pages/components.vue.js
+++ Src/Pages/components.vue.js
@@ -1,11 +1,13 @@
 const Components = Vue.component('Components', {
   data: function () {
     return {
-			
+			openbars: false
     }
   },
 	methods:{
-		
+		changeOpenBars(){
+			this.openbars = !this.openbars
+		}
 	},
   template: `
 	<div class="component components">
@@ -19,6 +21,16 @@ const Components = Vue.component('Components', {
 			<input type="checkbox">
 			<span class="dk-slider round"></span>
 		</label>
+
+		<div id="nav-icon" :class="openbars ? 'open' : 'close'" @click="changeOpenBars()">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+		<!-- I use this base https://codepen.io/designcouch/pen/Atyop -->
 	</div>
 	`
 })
